08776da3412da1141f82d803652c83440068fba9
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-19 04:05:38 +0300

    Block scope var hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-    task = false;
-    task2 = true;
-
+    const task = false;
+    const task2 = true;
+  
     if (trueOrFalse) {
-        var task = true;
-        var task2 = false;
+      const task = true;
+      const task2 = false;
+      if (task && task2) {
+        return [task, task2];
+      }
     }
-
+  
     return [task, task2];
-}
+  }
