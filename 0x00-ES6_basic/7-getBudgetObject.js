76ae91e8db08c628cc553fcfa0e0b6473ddc5ac8
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-19 18:21:10 +0300

    object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
---
+++ 0x00-ES6_basic/7-main.js
@@ -0,0 +1,3 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+console.log(getBudgetObject(400, 700, 900));
