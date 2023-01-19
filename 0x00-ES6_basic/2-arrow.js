4ce8a9def9b0d36ad8e18b07f630c2468cd22953
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-19 04:07:47 +0300

    arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,7 @@
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
+    const self = this;
     this.addNeighborhood = (newNeighborhood) => {
       self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
