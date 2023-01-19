1f4f8d0500692404d18f188a86ec190d3942ddef
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-19 04:39:31 +0300

    default parameters

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,4 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+	return initialNumber + expansion1989 + expansion2019;
+}
+
---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,6 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
+
