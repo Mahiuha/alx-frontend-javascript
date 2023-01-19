03df0261d8454a58d1efd06e2f41bdb9bc542f94
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-19 04:18:38 +0300

    Arrow functions

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-      if (task && task2) {
-        return [task, task2];
-      }
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
     }
-  
-    return [task, task2];
   }
+
+  return [task, task2];
+}
--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,4 +1,4 @@
-import taskBlock from "./1-block-scoped";
+import taskBlock from './1-block-scoped';
 
 console.log(taskBlock(true));
 console.log(taskBlock(false));
