e5781c2c2999c318ba6443dd1afcfaf2a88430a0
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-20 04:37:20 +0300

    method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,12 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
---
+++ 0x00-ES6_basic/9-main.js
@@ -0,0 +1,6 @@
+import getFullBudgetObject from './9-getFullBudget.js';
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
