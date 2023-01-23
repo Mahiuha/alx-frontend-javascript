f4c463c18459cabe2432ff1b35bd4be1a13d34a3
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-23 21:21:05 +0300

    iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,8 @@
 export default function createIteratorObject(report) {
-  // Define the iterator
-  let index = 0;
-  return {
-    next() {
-      if (index < report.employees.length) {
-        const value = report.employees[index];
-        index += 1;
-        return { value, done: false };
-      }
-      return { done: true };
-    },
-  };
+  let result = [];
+  for (const value of Object.values(report.allEmployees)) {
+    result = [...result, ...value];
+  }
+
+  return result;
 }
