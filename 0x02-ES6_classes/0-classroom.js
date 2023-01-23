8fdacdc3334c19cd7fadce3caeadd7a264eddcff
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-24 00:57:52 +0300

    You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
+
---
+++ 0x02-ES6_classes/0-main.js
@@ -0,0 +1,5 @@
+import ClassRoom from "./0-classroom";
+
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize)
+
