e260b73b988b0c545361083c30e77305db4ba71d
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-23 20:36:03 +0300

    only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,8 @@
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The fake API is not working currently'));
+  });
 }
+
+export default getFullResponseFromAPI;
