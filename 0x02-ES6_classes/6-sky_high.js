6b24b832be1f2cfedc0e25175b4392972db68463
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-24 05:26:01 +0300

    code refactor

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,18 +3,21 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
