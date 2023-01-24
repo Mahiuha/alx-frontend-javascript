48066baeec63280d5fefaca72ffa2a477b863f5f
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-24 03:08:23 +0300

    inheritance

---
+++ 0x02-ES6_classes/6-main.js
@@ -0,0 +1,6 @@
+import SkyHighBuilding from './6-sky_high';
+
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
