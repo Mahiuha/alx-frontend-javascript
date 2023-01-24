d1ddbb04379580cfa3246809c6e755b7039bbc29
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-24 05:31:52 +0300

    evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,12 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
