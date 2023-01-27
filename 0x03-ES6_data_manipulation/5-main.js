afa5c22312e0a7dd3de2a7b6c0a669eac9771fcb
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-27 22:23:54 +0300

    typed arrays

---
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -0,0 +1,3 @@
+import createInt8TypedArray from './5-typed_arrays';
+
+console.log(createInt8TypedArray(10, 2, 89));
---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  view[position] = value;
+  return buffer;
+}
