ac303613f549902d8bf77b13f22f75c6661c87af
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-27 22:53:36 +0300

    weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -0,0 +1,15 @@
+import { queryAPI, weakMap } from './100-weak';
+
+const endpoint = { protocol: 'http', name: 'getUsers' };
+weakMap.get(endpoint);
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
