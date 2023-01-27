fdb730a02882575130bc595354706503288b6411
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-27 22:38:36 +0300

    clean set

---
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -0,0 +1,4 @@
+import cleanSet from './8-clean_set';
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
