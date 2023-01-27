071af9f2144590ea2f39550bc3361ea9e9851c8a
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-27 22:34:43 +0300

    more set data structure

---
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -0,0 +1,5 @@
+import hasValuesFromArray from './7-has_array_values';
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
