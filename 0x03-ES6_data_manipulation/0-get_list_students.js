c0522456029dfbc8c4f95a8977336022c00ab3fd
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-28 03:13:07 +0300

    basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,7 @@
 export default function getListStudents() {
   return [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 3,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
