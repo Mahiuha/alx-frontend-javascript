ce81ba6bfa781d20e78efcb9014b686c7e3f0dc5
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-31 04:26:20 +0300

    student and teachers interface

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,37 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+  location: "New York"
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 23,
+  location: "London"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+function renderTable() {
+  const table = document.createElement("table");
+  studentsList.forEach(student => {
+    const row = table.insertRow();
+    const firstNameCell = row.insertCell();
+    firstNameCell.innerHTML = student.firstName;
+    const locationCell = row.insertCell();
+    locationCell.innerHTML = student.location;
+  });
+  document.body.appendChild(table);
+}
+
+renderTable();
+
