7dec0ad1cce0946ac9a1aae78d6afbd7cf7752fb
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-31 04:47:21 +0300

    writting class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,43 +1,75 @@
 interface Teacher {
-  // first name of the teacher
-  firstName: string;
-  // last name of the teacher
-  lastName: string;
-  // whether the teacher is a full-time employee
-  fullTimeEmployee: boolean;
-  // years of experience of the teacher
-  yearsOfExperience?: number;
-  // location of the teacher
-  location: string;
-  // allow to add any additional attribute to the object
-  [key: string]: any;
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    location: string;
+    yearsOfExperience?: number;
+    [key: string]: any
 }
 
-// extend the Teacher interface to create a new interface Directors
 interface Directors extends Teacher {
-  // number of reports the director manages
-  numberOfReports: number;
+    numberOfReports: number;
 }
 
-// initialize the first director object
+const teacher1: Teacher = {
+    firstName: 'Frank',
+    lastName: 'Donald',
+    fullTimeEmployee: true,
+    location: 'New Jersey',
+    contract: false,
+}
+
+console.log(teacher1);
+
 const director1: Directors = {
-  firstName: "John",
-  lastName: "Doe",
-  fullTimeEmployee: true,
-  yearsOfExperience: 5,
-  location: "New York",
-  numberOfReports: 10
-};
+    firstName: 'John',
+    lastName: 'Smith',
+    location: 'America',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
 
-// initialize the second director object
-const director2: Directors = {
-  firstName: "Jane",
-  lastName: "Doe",
-  fullTimeEmployee: false,
-  location: "London",
-  numberOfReports: 20
-};
+console.log(director1);
 
-// print the second director object
-console.log(director2);
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
+
+console.log(printTeacher('john', 'doe'));
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+    new(firstName: string, lastName: string): StudentClassInterface; 
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+function createStudent(cStudent: StudentClassConstructorInterface, firstName: string, lastName: string): StudentClassInterface {
+    return new cStudent(firstName, lastName);
+}
+
+const student1 = createStudent(StudentClass, 'mike', 'tyson');
+console.log(student1);
 
