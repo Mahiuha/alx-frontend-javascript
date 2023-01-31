d7dfb9f878f997517284cf37e3fdb4d34ce5fb29
Mahiuha <tiasho.mahiuha@gmail.com>
2023-01-31 05:16:05 +0300

    ambient namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+import { RowID, RowElement } from './interface';
+
+declare module 'crud' {
+  export function insertRow(row: RowElement): RowID;
+  export function deleteRow(rowId: RowID): void;
+  export function updateRow(rowId: RowID, row: RowElement): RowID;
+}
+
